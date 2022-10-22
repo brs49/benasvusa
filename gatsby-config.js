ee68d34ded9ab0108503bb410a958e5a612d5278
Brs <80780786+brs49@users.noreply.github.com>
2022-10-22 21:34:45 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,9 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Benas vusa`,
-    siteTitleAlt: `Benas vusa`,
-    siteHeadline: `Benas vusa`,
+    siteTitle: `Snaebavusa`,
+    siteTitleAlt: `Snaebavusa`,
+    siteHeadline: `Snaebavusa`,
     siteUrl: `https://benasvusa.wordpress.com/`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
     siteLanguage: `en`,
