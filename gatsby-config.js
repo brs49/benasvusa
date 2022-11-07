94662f8f60e1f8571e98c0386e2766af56629023
Brs <80780786+brs49@users.noreply.github.com>
2022-11-07 11:24:13 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   plugins: [
     {
-      
+      resolve: `@lekoarts/gatsby-theme-cara`,
       // See the theme's README for all available options
       options: {},
     },
